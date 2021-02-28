5a3824c593fd40752b21ce35ea6d2a020c69be4a
Danny <mezenov.201521@gmail.com>
2021-02-28 20:14:15 +0300

    init_mock

---
+++ src/test/java/com/devilpanda/nota_tabula/NotaTabulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devilpanda.nota_tabula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaTabulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
